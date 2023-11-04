4fffd3dbfb9c8a196651c3b2de241b7ae36c6343
SamuelParrales <samuel.parrales@outlook.com>
2023-11-03 19:20:17 -0500

    width card img

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+// console.log(process.env.NODE_ENV)
+const base = process.env.NODE_ENV == 'development'
+              ?''
+              :'/react-digimon-app/'
 export default defineConfig({
   plugins: [react()],
-  // base: '/react-digimon-app/',
+  base,
+  
 })
 
